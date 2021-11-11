460f3cb0429e7ad7ec1cef9bb5cb3ad3fa20ef02
Stefan Pekez <stefanpekez00@gmail.com>
2021-11-11 16:42:21 +0100

    Finished UI for the main page

--- app/src/main/java/com/example/iksoksandroidapp/MainActivity.java
+++ app/src/main/java/com/example/iksoksandroidapp/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.iksoksandroidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void openSettingsActivity (View view) {
+        Intent enterSettings = new Intent(this, SettingsActivity.class);
+        startActivity(enterSettings);
+    }
+
+    public void onButtonClassicClick (View view) {
+
+    }
+
+    public void onButtonCustomClick (View view) {
+
+    }
+
 }
